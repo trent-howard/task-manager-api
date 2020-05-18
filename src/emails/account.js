656d805864aebc4f2e7496d73b0d27ca8a702d0f
Trent Howard <trent.howard.42@gmail.com>
2020-05-18 17:53:24 +1000

    Remove hardcoded email address

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,12 +1,13 @@
 const sgMail = require('@sendgrid/mail')
 const sendgridKey = process.env.SENDGRID_API_KEY
+const fromAddress = process.env.FROM_ADDRESS
 
 sgMail.setApiKey(sendgridKey)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: ***REMOVED***,
+        from: fromAddress,
         subject: 'Welcome to the task app!',
         text: `Welcome to the task app, ${name}. Let me know how you get on!`
     })
@@ -15,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendAccountCloseEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: ***REMOVED***,
+        from: fromAddress,
         subject: 'Sorry to see you go',
         text: `We hate to see you leave ${name}, but we'd be ever so greatful if you took
          a moment to tell us what we could do better.`
